3a98eafea2709c23de5e3590b00148d0bf5be39f
Anbarasan <anbarasanmarimuthu01@gmail.com>
2024-09-03 23:51:04 +0530

    changing vercel.json

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return f'Hello, Flask on Vercel!'
+    return f'<h1>Hello, I am Anbarasan</h1>'
 
 if __name__ == "__main__":
     app.run()
